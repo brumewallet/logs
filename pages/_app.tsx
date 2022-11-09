0e3db807da72f60c3a86f755ae5a9b3c59d70221
Haz Æ 41 <4405263+hazae41@users.noreply.github.com>
2022-11-09 16:04:24 +0100

    logs

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,9 @@
-import '../styles/globals.css'
+import { XSWR } from '@hazae41/xswr'
 import type { AppProps } from 'next/app'
+import '../styles/globals.css'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <XSWR.CoreProvider>
+    <Component {...pageProps} />
+  </XSWR.CoreProvider>
 }
---
+++ pages/api/logs.ts
@@ -0,0 +1,20 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { createClient } from '@supabase/supabase-js'
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+const supabaseUrl = process.env.SUPABASE_URL!
+const supabaseKey = process.env.SUPABASE_KEY!
+const supabase = createClient(supabaseUrl, supabaseKey)
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { data, error } = await supabase
+    .from("requests")
+    .select("*")
+    .order("time", { ascending: false })
+    .limit(100)
+
+  res.status(200).json({ data, error })
+}
