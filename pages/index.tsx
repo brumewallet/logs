e1d9ba1c29d56acbe63dece5fa7e5de87a943476
ChocoDev23 <chocowalletdev@proton.me>
2022-11-12 23:54:50 +0100

    fix ip

--- pages/index.tsx
+++ pages/index.tsx
@@ -44,13 +44,29 @@ export default function Home() {
       </span>
     </div>
 
+  const ipArray = [
+    "199.249.230.140",
+    "2620:7:6001::140",
+    "185.16.38.111",
+    "104.244.73.85",
+    "2605:6400:30:f71c:7d7a:8bf1:dead:beef",
+    "5.45.106.207",
+    "2a03:4000:6:16f9:b4d0:68ff:feeb:cf35",
+    "107.189.12.148",
+    "192.42.116.27",
+    "2001:67c:6ec:203:218:33ff:fe44:5527",
+    "51.15.250.93",
+    "185.220.101.62",
+    "2a0b:f4c2:2::62",
+  ]
+
   const checkTorLog = (log: Log) => {
-    if (log.ip === "2620:7:6001::140") return true
+    if (ipArray.includes(log.ip)) return true
     return false
   }
 
   const checkOtherLog = (log: Log) => {
-    if (log.ip !== "2620:7:6001::140") return true
+    if (!ipArray.includes(log.ip)) return true
     return false
   }
 
