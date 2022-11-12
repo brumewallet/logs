7bc789a6f59ce33294222312cb8c37b551ae1a91
ChocoDev23 <chocowalletdev@proton.me>
2022-11-13 00:11:01 +0100

    fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -64,8 +64,15 @@ export default function Home() {
         </span>
         <div className="my-2" />
         {logs.data?.filter(checkTorLog).map(log =>
-          <div key={log.time} className="p-4 border rounded-xl bg-component border border-default w-[400px]">
-            <a className="absolute left-[600px]"
+          <div key={log.time} className="p-4 flex items-start border rounded-xl bg-component border border-default w-[400px]">
+            <div className="flex flex-col">
+              {LogSubrow("Time", new Date(log.time).toLocaleString())}
+              {LogSubrow("IP Address", log.ip, "text-green-500")}
+              {LogSubrow("RPC Method", log.method)}
+              {LogSubrow("Endpoint", log.endpoint)}
+            </div>
+            <div className="grow" />
+            <a
               href="https://metrics.torproject.org/rs.html#details/A868303126987902D51F2B6F06DD90038C45B119"
               target="_blank" rel="noopener noreferrer">
               <OppositeTextButtonRounded>
@@ -73,10 +80,6 @@ export default function Home() {
                   src="tor.svg" alt="tor-logo" />
               </OppositeTextButtonRounded>
             </a>
-            {LogSubrow("Time", new Date(log.time).toLocaleString())}
-            {LogSubrow("IP Address", log.ip, "text-green-500")}
-            {LogSubrow("RPC Method", log.method)}
-            {LogSubrow("RPC Endpoint", log.endpoint)}
           </div>)}
       </div>
       <div className="flex flex-col items-center gap-2">
@@ -85,8 +88,15 @@ export default function Home() {
         </span>
         <div className="my-2" />
         {logs.data?.filter(checkOtherLog).map(log =>
-          <div key={log.time} className="p-4 border rounded-xl bg-component border border-default w-[400px]">
-            <a className="absolute right-[300px]"
+          <div key={log.time} className="p-4 flex items-start border rounded-xl bg-component border border-default w-[400px]">
+            <div className="flex flex-col">
+              {LogSubrow("Time", new Date(log.time).toLocaleString())}
+              {LogSubrow("IP Address", log.ip, "text-red-500")}
+              {LogSubrow("RPC Method", log.method)}
+              {LogSubrow("Endpoint", log.endpoint)}
+            </div>
+            <div className="grow" />
+            <a
               href="https://www.iplocation.net/"
               target="_blank" rel="noopener noreferrer">
               <OppositeTextButtonRounded>
@@ -94,10 +104,6 @@ export default function Home() {
                   src="ip.png" alt="My-ip" />
               </OppositeTextButtonRounded>
             </a>
-            {LogSubrow("Time", new Date(log.time).toLocaleString())}
-            {LogSubrow("IP Address", log.ip, "text-red-500")}
-            {LogSubrow("RPC Method", log.method)}
-            {LogSubrow("RPC Endpoint", log.endpoint)}
           </div>)}
       </div>
     </div>
