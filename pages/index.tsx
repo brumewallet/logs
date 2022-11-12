6a434dac9db0b12efdc01510458b45405f9f9ad4
ChocoDev23 <chocowalletdev@proton.me>
2022-11-12 23:55:27 +0100

    fix2

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import { getSingleSchema, Result, useFetch, useQuery } from "@hazae41/xswr"
 import { OppositeTextButton, OppositeTextButtonRounded } from "../components/buttons/button"
+import { ipArray } from "../react/utils"
 
 async function fetcher<T>(url: string) {
   const res = await fetch(url)
@@ -44,21 +45,6 @@ export default function Home() {
       </span>
     </div>
 
-  const ipArray = [
-    "199.249.230.140",
-    "2620:7:6001::140",
-    "185.16.38.111",
-    "104.244.73.85",
-    "2605:6400:30:f71c:7d7a:8bf1:dead:beef",
-    "5.45.106.207",
-    "2a03:4000:6:16f9:b4d0:68ff:feeb:cf35",
-    "107.189.12.148",
-    "192.42.116.27",
-    "2001:67c:6ec:203:218:33ff:fe44:5527",
-    "51.15.250.93",
-    "185.220.101.62",
-    "2a0b:f4c2:2::62",
-  ]
 
   const checkTorLog = (log: Log) => {
     if (ipArray.includes(log.ip)) return true
