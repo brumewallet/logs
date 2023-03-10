31c5643b1d21ab14be0808441d482a28ba7a27ae
ChocoDev23 <chocowalletdev@proton.me>
2023-03-10 19:40:28 +0100

    Name

--- pages/index.tsx
+++ pages/index.tsx
@@ -78,7 +78,7 @@ export default function Home() {
     <div className="flex items-start justify-center gap-[100px]">
       <div className="flex flex-col items-center gap-2">
         <span className="text-3xl text-colored">
-          Lunar requests
+          Brume requests
         </span>
         <div className="my-2" />
         {logs.data?.filter(checkTorLog).map(log =>
@@ -142,7 +142,7 @@ export default function Home() {
   const Header = <div className="flex flex-col items-center">
     <div className="flex justify-center items-center gap-4">
       <span className="text-5xl text-center text-colored">
-        Lunar Wallet
+        Brume Wallet
       </span>
       <img className="icon-3xl"
         src="logo.svg" alt="logo" />
